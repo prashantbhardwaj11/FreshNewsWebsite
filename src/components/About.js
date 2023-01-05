4e809508d4c7399fc3eb4d9269a3285cf20606d3
Prashant11 <itsbhardwaj11@gmail.com>
2023-01-05 16:17:25 +0530

    changing length

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,8 @@ class componentName extends Component {
     }
     return (
       <>
-        <div className="container my-4 " >
+
+        <div className="container my-5" >
           <p style={styleObj} >This Webiste Created By  Ravi Sharma!
             You can use this website And if u have any suggestions so plz mail me at ravisharmars8859@gmail.com Thanks ....    Credited to Code With Harry and <a  href="https://icons8.com/icon/WYwcSbCMk4UC/news">News</a> icon by <a  href="https://icons8.com">Icons8</a></p>
       
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ class NewsItem extends Component {
     let { title, desc ,imgUrl ,CompleteNews} = this.props;
     return (
         <>
-        <div className="my-3">
+        <div className="my-4">
         <div className="card">
         <img src={!imgUrl?"https://derivates.kicker.de/image/upload/c_crop,x_0,y_245,w_2394,h_1347/w_1200,q_auto/v1/2022/01/19/45b94278-eb6e-4759-8b2a-4528dd5d6cf2.jpeg":imgUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
